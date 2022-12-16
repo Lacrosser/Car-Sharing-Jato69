fcc05ec3a4521dc040fee594530c442997cb8d56
LorenzoTo02 <lorenzozeppegno@gmail.com>
2022-12-16 14:31:31 +0100

    Creato script per portare le api dei veicoli

--- Project-work/js/script.js
+++ Project-work/js/script.js
@@ -1,1 +1,26 @@
-var c = 0;
+// api url
+const api_url = "http://localhost:3000/Veicoli";
+
+var mezzoProva = document.querySelector('#mezzo1');
+
+var contatore = 1;
+
+function mostraVeicoli(listaVeicoli){
+    listaVeicoli.forEach(veicolo => {
+        var mezzo = veicolo.mezzo;
+        var colore = veicolo.colore;
+        // var id = veicolo.id;
+        
+        mezzoProva.innerHTML += "<li>Veicolo " + contatore + ": "+ mezzo + " " + colore + '</li>';
+        contatore++;
+    });
+}
+
+fetch(api_url)
+.then(data => {
+    return data.json()
+})
+.then(response => {
+    console.log(typeof response)
+    mostraVeicoli(response)
+});
