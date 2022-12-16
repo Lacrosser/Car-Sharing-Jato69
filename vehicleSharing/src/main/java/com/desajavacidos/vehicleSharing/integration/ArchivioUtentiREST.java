da56759996fe77a8bd64a85309f214d059704a5a
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-16 15:34:18 +0100

    Archivio Merge

--- vehicleSharing/src/main/java/com/desajavacidos/vehicleSharing/integration/ArchivioUtentiREST.java
+++ vehicleSharing/src/main/java/com/desajavacidos/vehicleSharing/integration/ArchivioUtentiREST.java
@@ -3,22 +3,49 @@ package com.desajavacidos.vehicleSharing.integration;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desajavacidos.vehicleSharing.entities.ArchivioUtenti;
-import com.desajavacidos.vehicleSharing.service.ArchivioUtenteServiceImpl;
+import com.desajavacidos.vehicleSharing.service.iService.ArchivioUtentiService;
 
 @RestController
 @RequestMapping("api")
 public class ArchivioUtentiREST {
+
 	@Autowired
-	private ArchivioUtenteServiceImpl service;
+	private ArchivioUtentiService service;
 	
-	@GetMapping("utenti")
+	@GetMapping("archivioutenti")
 	public List<ArchivioUtenti> getAll(){
 		return service.getAll();
 	}
-
+	
+	@GetMapping("archivioutenti/{id}")
+	public ArchivioUtenti getUtente(@PathVariable("id") int id) {
+		return service.getUtenteById(id);
+	}
+	
+	@PostMapping("archivioutenti")
+	public void addUtente(@RequestBody ArchivioUtenti u) {
+		service.addUtente(u);
+	}
+	
+	@PutMapping("archivioutenti")
+	public void updateUtente(@RequestBody ArchivioUtenti u) {
+		service.updateUtente(u);
+	}
+	
+	@DeleteMapping("archivioutenti/{id}")
+	public void deleteUtente(@PathVariable("id") int id) {
+		service.deleteUtenteById(id);
+	}
+	
+	
 }
