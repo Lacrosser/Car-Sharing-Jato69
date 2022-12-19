4821cb78fec498ec38b1921e937c802d69e45461
mundaca97 <114602160+mundaca97@users.noreply.github.com>
2022-12-19 18:43:40 +0100

    test

--- vehicleSharing/src/test/java/com/desajavacidos/vehicleSharing/VehicleSharingApplicationTests.java
+++ vehicleSharing/src/test/java/com/desajavacidos/vehicleSharing/VehicleSharingApplicationTests.java
@@ -1,13 +1,41 @@
 package com.desajavacidos.vehicleSharing;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.desajavacidos.vehicleSharing.entities.ArchivioUtenti;
+import com.desajavacidos.vehicleSharing.services.iServices.ArchivioUtentiService;
+import com.desajavacidos.vehicleSharing.services.iServices.DescrizioneService;
+import com.desajavacidos.vehicleSharing.services.iServices.PrenotazioneService;
+import com.desajavacidos.vehicleSharing.services.iServices.VeicoliService;
 
 @SpringBootTest
 class VehicleSharingApplicationTests {
 
+	@Autowired
+	ArchivioUtentiService utente;
+	@Autowired
+	DescrizioneService descrizione;
+	@Autowired
+	PrenotazioneService prenotazione;
+	@Autowired
+	VeicoliService veicolo;
+	
 	@Test
 	void contextLoads() {
+		
+		ArchivioUtenti u = new ArchivioUtenti();
+		u.setCognome("Rossi");
+		u.setNome("Mario");
+		u.setEmail("mario.rossi@gmail.com");
+		u.setPassword("mariorossi");
+		u.setTipo("A");
+		u.setUserId("MARIOROSSI");
+		
+		System.out.println("Non Aggiunto");
+		utente.addUtente(u);
+		System.out.println("Aggiunto");
 	}
 
 }
