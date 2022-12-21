6fb7db7fb4492d65a78a4b09d33ca8d3f9a4d27d
rebooted943 <116455857+rebooted943@users.noreply.github.com>
2022-12-21 12:20:27 +0100

    test banner e swiper + clipper

--- Project-work/js/script.js
+++ Project-work/js/script.js
@@ -40,3 +40,25 @@ fetch(api_url)
     mostraVeicoli(response)
 });
 // Prova Modifica
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
