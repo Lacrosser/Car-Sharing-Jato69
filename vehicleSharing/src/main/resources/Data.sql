c08d26ab76072b3522b2d2582913a5500cbb35d4
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-22 18:26:08 +0100

    Update Data.sql

--- vehicleSharing/src/main/resources/Data.sql
+++ vehicleSharing/src/main/resources/Data.sql
@@ -21,7 +21,7 @@
 
 LOCK TABLES `archivioutenti` WRITE;
 /*!40000 ALTER TABLE `archivioutenti` DISABLE KEYS */;
-INSERT INTO `archivioutenti` VALUES (1,'Paperino','2005-12-31 22:00:00.000000','paono.paperino@paperopoli.com ','Amministratore dei servizi','20/08/1900','Paolino','Amministratore','A',NULL,'Amministratore'),(2,'Paperino','2005-12-31 22:00:00.000000','paolino.paperino@paperopoli.com ','Amministratore dei servizi','20/08/1900','Paolino','Amministratore','A',NULL,'Amministratore'),(3,'Paperino','2005-12-31 22:00:00.000000','paolina.paperino@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente1','B',NULL,'Utente1'),(4,'Rossi','2005-12-31 22:00:00.000000','paalino.rossi@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente2','B',NULL,'Utente2'),(5,'Verdi','2005-12-31 22:00:00.000000','pawlino.verdi@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente3','B',NULL,'Utente3'),(6,'Bianchi','2005-12-31 22:00:00.000000','paelino.bianchi@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente4','B',NULL,'Utente4'),(7,'Neri','2005-12-31 22:00:00.000000','plino.neri@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente5','B',NULL,'Utente5');
+INSERT INTO `archivioutenti` VALUES (1,'Paperino','2005-12-31 22:00:00.000000','paono.paperino@paperopoli.com ','Amministratore dei servizi','20/08/1900','Paolino','Amministratore','A',NULL,'Amministrator'),(2,'Paperino','2005-12-31 22:00:00.000000','paolino.paperino@paperopoli.com ','Amministratore dei servizi','20/08/1900','Paolino','Amministratore','A',NULL,'Amministratore'),(3,'Paperino','2005-12-31 22:00:00.000000','paolina.paperino@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente1','B',NULL,'Utente1'),(4,'Rossi','2005-12-31 22:00:00.000000','paalino.rossi@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente2','B',NULL,'Utente2'),(5,'Verdi','2005-12-31 22:00:00.000000','pawlino.verdi@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente3','B',NULL,'Utente3'),(6,'Bianchi','2005-12-31 22:00:00.000000','paelino.bianchi@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente4','B',NULL,'Utente4'),(7,'Neri','2005-12-31 22:00:00.000000','plino.neri@paperopoli.com ','Utente dei servizi','20/08/1900','Paolino','Utente5','B',NULL,'Utente5');
 /*!40000 ALTER TABLE `archivioutenti` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -31,7 +31,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `prenotazione` WRITE;
 /*!40000 ALTER TABLE `prenotazione` DISABLE KEYS */;
-INSERT INTO `prenotazione` VALUES (1,NULL,2,1),(2,NULL,3,2),(3,NULL,4,4),(4,NULL,5,4);
+INSERT INTO `prenotazione` VALUES (1,NULL,2,1),(2,NULL,3,2),(3,NULL,4,3),(4,NULL,5,4),(5,NULL,6,5);
 /*!40000 ALTER TABLE `prenotazione` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -41,7 +41,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `veicoli` WRITE;
 /*!40000 ALTER TABLE `veicoli` DISABLE KEYS */;
-INSERT INTO `veicoli` VALUES (1,'elettrica',NULL,'peppino',_binary '',NULL,'Roma',_binary '','AUTO'),(2,'elettrica',NULL,'peppino',_binary '',NULL,'Roma',_binary '','MONOPATTINO'),(3,'elettrica',NULL,'peppino',_binary '',NULL,'Roma',_binary '','BICICLETTA'),(4,'elettrica',NULL,'peppino',_binary '\0',NULL,'Roma',_binary '','AUTO'),(5,'elettrica',NULL,'peppino',_binary '\0',NULL,'Roma',_binary '','MONOPATTINO'),(6,'elettrica',NULL,'peppino',_binary '\0',NULL,'Roma',_binary '','BICICLETTA');
+INSERT INTO `veicoli` VALUES (1,'elettrica','45Cv','Bianca',NULL,_binary '',NULL,'VOLVO',NULL,_binary '','AUTO'),(2,'ibrida','95Cv','Nera',NULL,_binary '\0',NULL,'Volvo',NULL,_binary '\0','BICICLETTA'),(3,'elettrica','50Cv','Verdi',NULL,_binary '\0',NULL,'Mercerdes',NULL,_binary '','MONOPATTINO'),(4,'elettrica','5Cv','ROSA',NULL,_binary '',NULL,'Volvo',NULL,_binary '\0','MONOPATTINO'),(5,'Ibrida','415Cv','Fuxia',NULL,_binary '',NULL,'Volvo',NULL,_binary '','AUTO');
 /*!40000 ALTER TABLE `veicoli` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-21 17:05:33
+-- Dump completed on 2022-12-22 18:25:39
