2a792b00bbb8eb614afe375526d6dc8b358ab568
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-22 16:31:13 +0100

    Update ArchivioUtentiREST.java

--- vehicleSharing/src/main/java/com/desajavacidos/vehicleSharing/integration/ArchivioUtentiREST.java
+++ vehicleSharing/src/main/java/com/desajavacidos/vehicleSharing/integration/ArchivioUtentiREST.java
@@ -7,7 +7,6 @@ import com.desajavacidos.vehicleSharing.services.iServices.ArchivioUtentiService
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
