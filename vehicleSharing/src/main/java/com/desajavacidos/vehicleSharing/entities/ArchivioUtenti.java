00ee65f83f6eb4aa9d90896152f16de0e8acc2b6
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-22 15:20:32 +0100

    utenti

--- vehicleSharing/src/main/java/com/desajavacidos/vehicleSharing/entities/ArchivioUtenti.java
+++ vehicleSharing/src/main/java/com/desajavacidos/vehicleSharing/entities/ArchivioUtenti.java
@@ -5,10 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- vehicleSharing/src/main/java/com/desajavacidos/vehicleSharing/integration/ArchivioUtentiREST.java
+++ vehicleSharing/src/main/java/com/desajavacidos/vehicleSharing/integration/ArchivioUtentiREST.java
@@ -4,17 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.desajavacidos.vehicleSharing.entities.ArchivioUtenti;
 import com.desajavacidos.vehicleSharing.services.iServices.ArchivioUtentiService;
-import com.desajavacidos.vehicleSharing.services.iServices.PrenotazioneService;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
