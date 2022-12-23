261a052ee025b50ce0e596bce921190ace90515f
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-23 16:16:37 +0100

    update merge

--- vehicleSharing/src/main/resources/static/js/scriptLogin.js
+++ vehicleSharing/src/main/resources/static/js/scriptLogin.js
@@ -5,54 +5,6 @@ let bottone = document.querySelector("#loginButton");
 
 
 
-// let body ={
-//     userId:username,
-//     password : password
-// };
-
-
-// function loggare() {
-
-//     //let risposta = new body(username, password);
-
-//     fetch(LOGIN,{
-//         method: "POST",
-
-//         body:JSON.stringify(body),
-//         headers: {
-//             'Content-Type': 'application/json'
-//           }
-
-//     })
-
-//         .then(respose => respose.json())
-//         .then(data => {
-//             var tipos = data.tipo;
-//             console.log(tipos);
-//             // if (data.tipo === "A" || data.tipo === "B") {
-
-//             //     var tipo = data.tipo;
-
-//             //     console.log(tipo);
-
-//             //     sessionStorage.setItem("username", username);
-//             //     sessionStorage.setItem("password", password);
-//             //     sessionStorage.setItem("tipo", tipo);
-
-//             //     return true;
-//             // } else {
-
-//             //     console.log("utente C")
-//             //     sessionStorage.setItem("tipo", tipo);
-
-//             //     return false;
-//             // }
-
-//         })
-
-// }
-
-
 // Invia la richiesta POST al server
 function log2() {
     let username = document.querySelector("#username").value;
