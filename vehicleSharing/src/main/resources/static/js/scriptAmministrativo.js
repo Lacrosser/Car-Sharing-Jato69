f5fba9aaf9a4564eb2699d6fccc2564bb326e9e3
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-23 20:29:06 +0100

    Update scriptAmministrativo.js

--- vehicleSharing/src/main/resources/static/js/scriptAmministrativo.js
+++ vehicleSharing/src/main/resources/static/js/scriptAmministrativo.js
@@ -1,1 +1,12 @@
 //pagina di script per il check di utente
+
+function checkConnection(){
+
+if(sessionStorage.getItem("username")!=null &&sessionStorage.getItem("password")){
+
+
+    
+}
+
+    
+}
