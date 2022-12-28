9101a8c9951d4d9f0176110506700a8403cf20b0
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-28 17:35:41 +0100

    registrazione da checckare regexp

--- vehicleSharing/src/main/resources/static/js/Registrazione.js
+++ vehicleSharing/src/main/resources/static/js/Registrazione.js
@@ -3,6 +3,10 @@ const UTENTE = "Utente dei servizi"
 const TIPO = "B"
 
 
+const REGUSER = /^[a-zA-Z\d]{1,12}$/;
+const REGPASS = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
 /* -------------------------------------------------------------------------- */
 /*                                  Variabili                                 */
 /* -------------------------------------------------------------------------- */
@@ -18,7 +22,8 @@ let dataNascita = document.querySelector("#dataNascita");
 
 
 let btnReg = document.querySelector("#registrati")
-let errorMsg = document.querySelector("#errorIns");
+let errorUser = document.querySelector("#errorUser");
+let errorPass = document.querySelector("#errorPass");
 
 /* -------------------------------------------------------------------------- */
 /*                         AVVIO PAGINA/EVENT LISTENER                        */
@@ -40,8 +45,8 @@ function checkPassword() {
 
         return true;
     } else {
-        errorMsg.textContent = "";
-        errorMsg.textContent = "Le due password non coincidono"
+        
+         errorPass.textContent = "Le due password non coincidono"
         return false;
     }
 
@@ -62,7 +67,7 @@ function creaUtente(userId, password, firma, tipo, nome, cognome, nascita, email
 
 
 function registrazione() {
-    if (checkPassword) {
+    if (checherReg) {
         let utente = new creaUtente(username.value, pass1.value, UTENTE, TIPO, nome.value, cognome.value, dataNascita.value, email.value)
 
         fetch(UTENTIMAPPING, {
@@ -90,4 +95,135 @@ function checkLogin() {
     if (sessionStorage.getItem("username") != null || localStorage.getItem("username") != null) {
         window.location.assign('/');
     }
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 CHECKREGEX                                 */
+/* -------------------------------------------------------------------------- */
+
+function checherReg() {
+
+    ripristinoCampi();
+
+    if (checkPassword) {
+        if (controllaUser && controllaPassword) {
+            return true;
+        } else {
+            return false
+        }
+
+    } else return false;
+
+}
+
+
+
+
+function controllaUser() {
+    if (!username.value.match(REGUSER)) {
+
+        errorUser.textContent = "Per favore controlla il tuo Username:";
+
+        for (let i = 0; i < 2; i++) {
+
+            switch (i) {
+                case 0:
+                    var li = document.createElement("li")
+                    li.textContent = "Username: massimo 16 caratteri";
+                    errorUser.appendChild(li);
+
+                    break;
+                case 1:
+                    var li = document.createElement("li")
+                    li.textContent = "Username: Non sono accettati spazi o caratteri speciali";
+                    errorUser.appendChild(li);
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+        event.preventDefault();
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+function controllaPassword() {
+    if (!pass1.value.match(REGPASS)) {
+        //almeno una maiuscola
+        //almeno una minuscola
+        //almeno un carattere speciale @$!%*?&
+        //lunghezza minima 8 caratteri
+        errorPass.textContent = "Per favore controlla la tua Password:";
+        for (let i = 0; i < 6; i++) {
+
+            switch (i) {
+                case 0:
+                    var li = document.createElement("li")
+                    li.textContent = "Password: deve avere almeno una maiuscola";
+                    errorPass.appendChild(li);
+
+                    break;
+                case 1:
+                    var li = document.createElement("li")
+                    li.textContent = "Password:Deve avere almeno una minuscola";
+                    errorPass.appendChild(li);
+                    break;
+                case 2:
+                    var li = document.createElement("li")
+                    li.textContent = "Password:Deve avere almeno un carattere speciale @$!%*?&";
+                    errorPass.appendChild(li);
+
+                    break;
+                case 3:
+                    var li = document.createElement("li")
+                    li.textContent = "Password: Deve contenere almeno un numero";
+                    errorPass.appendChild(li);
+
+                    break;
+                case 4:
+                    var li = document.createElement("li")
+                    li.textContent = "Password: Lunghezza minima 8 caratteri";
+                    errorPass.appendChild(li);
+
+                    break;
+                case 5:
+                    var li = document.createElement("li")
+                    li.textContent = "Password: Deve contenere almeno un numero";
+                    errorPass.appendChild(li);
+
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+        event.preventDefault();
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+function ripristinoCampi() {
+    var li = document.querySelectorAll("ul#errorPass li");
+    var lu = document.querySelectorAll("ul#errorUser li");
+    errorPass.textContent = "";
+    errorUser.textContent = "";
+    errorLegoCity.textContent = "";
+    li.forEach(element => {
+
+        element.remove();
+
+    });
+    lu.forEach(luele => {
+        luele.remove();
+    });
 }
