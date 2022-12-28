ed0c01b5085fbcd002281ca540237430ed66197d
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-28 13:55:53 +0100

    modifica registrazione
    Da testare

--- vehicleSharing/src/main/resources/static/js/Registrazione.js
+++ vehicleSharing/src/main/resources/static/js/Registrazione.js
@@ -1,14 +1,35 @@
 const UTENTIMAPPING = "http://localhost:9069/api/utenti";
-const UTENTE="Utente dei servizi"
-const TIPO="B"
+const UTENTE = "Utente dei servizi"
+const TIPO = "B"
 
 
-let pass1 = document.querySelector("#password");
-let pass2 = document.querySelector("#password2")
-let username = document.querySelector("#username");
-let email = document.querySelector("#email");
-let errorMsg=document.quer
+/* -------------------------------------------------------------------------- */
+/*                                  Variabili                                 */
+/* -------------------------------------------------------------------------- */
 
+
+let username = document.querySelector("#username").value;
+let pass1 = document.querySelector("#password").value;
+let pass2 = document.querySelector("#password2").value;
+let nome = document.querySelector("#nome").value;
+let cognome = document.querySelector("#cognome").value;
+let email = document.querySelector("#email").value;
+let dataNascita = document.querySelector("#dataNascita").value;
+
+
+let errorMsg = document.querySelector("#errorIns");
+
+/* -------------------------------------------------------------------------- */
+/*                                Avvio pagina                                */
+/* -------------------------------------------------------------------------- */
+
+window.addEventListener("load",checkLogin())
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  Funzioni                                  */
+/* -------------------------------------------------------------------------- */
 
 function checkPassword() {
 
@@ -16,20 +37,55 @@ function checkPassword() {
 
         return true;
     } else {
+        errorMsg.textContent = "";
+        errorMsg.textContent = "Le due password non coincidono"
         return false;
     }
 
 
 }
 
-function creaUtente(userId,password,firma,tipo,nome,cognome,nascita,email){
-    this.userId=userId;
-    this.password=password;
-    this.firma=firma;
-    this.tipo=tipo;
-    this.nome=nome;
-    this.cognome=cognome;
-    this.nascita=nascita;
-    this.email=email;
+function creaUtente(userId, password, firma, tipo, nome, cognome, nascita, email) {
+    this.userId = userId;
+    this.password = password;
+    this.firma = firma;
+    this.tipo = tipo;
+    this.nome = nome;
+    this.cognome = cognome;
+    this.nascita = nascita;
+    this.email = email;
 
 }
+
+
+function registrazione() {
+    if (checkPassword) {
+        let utente = new creaUtente(username, pass1, UTENTE, TIPO, nome, cognome, dataNascita, email)
+
+        fetch(UTENTIMAPPING, {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(utente)
+
+
+            })
+            .then(data => {
+                data.json();
+            })
+        console.log("Utente aggiunto")
+
+
+    }
+
+
+
+}
+
+function checkLogin(){
+    if(sessionStorage.getItem("username") != null || localStorage.getItem("username") != null){
+        window.location.assign('/');
+      }
+}
+
