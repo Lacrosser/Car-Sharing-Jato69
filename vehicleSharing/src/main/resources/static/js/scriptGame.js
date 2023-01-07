669488e0153c2b56eaeb3db64da8e28b79ff8c3c
LorenzoTo02 <lorenzozeppegno@gmail.com>
2023-01-07 16:57:14 +0100

    CSS e Responsive 100%

--- vehicleSharing/src/main/resources/static/js/scriptGame.js
+++ vehicleSharing/src/main/resources/static/js/scriptGame.js
@@ -11,7 +11,12 @@ var divPulsante = document.querySelector('#pulsante');
 
 var record = document.querySelector('#record');
 
-record.textContent ="Record: " + localStorage.getItem('puntiGioco') + " s";
+if(localStorage.getItem('puntiGioco') == null){
+    record.textContent ="Record: " + 0 + " s";
+} else {
+    record.textContent ="Record: " + localStorage.getItem('puntiGioco') + " s";
+}
+
 
 var punteggio = document.querySelector('#punti');
 
