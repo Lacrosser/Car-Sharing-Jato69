d3203d595bf15f27e7b30fb42ac2fffd4450560b
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2023-01-09 11:20:29 +0100

    porca puttana modale logout

--- vehicleSharing/src/main/resources/static/js/session.js
+++ vehicleSharing/src/main/resources/static/js/session.js
@@ -9,7 +9,7 @@ function nascondiroba() {
     if(sessionStorage.getItem("tipo") == "A"  || localStorage.getItem("tipo") == "A"){
       adminBuilder();
     }
-
+    modalBuilder();
     logOutButton();
 
 
@@ -32,21 +32,31 @@ function logOutButton() {
 
   let li = document.createElement('li');
   li.setAttribute("class", "nav-item ms-auto ms-lg-0");
+  li.setAttribute("data-bs-toggle", "modal");
+  li.setAttribute("data-bs-target", "#logoutModal");
   li.setAttribute("id", "logOut");
 
-  let a = document.createElement("a");
-  a.setAttribute("class", "nav-link");
-  a.setAttribute("href", "/");
-  a.textContent = "LogOut";
+  let span = document.createElement("span");
+  span.setAttribute("class", "nav-link logout");
+  
+  // span.setAttribute("href", "/");
+  span.textContent = "LogOut";
 
+  let navbarElement = document.querySelector("#navbar");
 
-  let navbarElement = document.querySelector("#navbar")
-
-  li.appendChild(a)
+  li.appendChild(span)
   navbarElement.appendChild(li);
+  
+  let logoutModale = document.querySelector("#logoutModal");
+
+ 
+    li.addEventListener("click", function() {
+        
+        logoutModale.style.display = 'block';
+    });
 
 
-  li.addEventListener('click', logOut);
+  // li.addEventListener('click', logOut);
 
 }
 
@@ -112,5 +122,54 @@ function loginBuilder() {
 
   li.appendChild(a)
   navbarElement.appendChild(li);
+
+}
+
+function modalBuilder(){
+  let htmlCode = '';
+
+  htmlCode=`<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+          <div class="modal-header bg-danger">
+              <h2 class="h1 modal-title fs-5 text-white text-bg-danger" id="titoloModale">Log Out</h2>
+              <button type="button" class="btn-close text-bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body" id="contenutoModale">
+              <p class="lead">Sei sicuro di voler sloggare?</p>
+
+              <p id="error" class="lead text-danger"></p>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Annulla</button>
+              <button id="logoutBottone" type="button" class="btn btn-danger" data-bs-dismiss="modal" value="LogOut">Si</button>
+          </div>
+      </div>
+  </div>
+</div>`
+
+
+
+let divLog=document.querySelector("#modLogout");
+
+divLog.innerHTML=htmlCode;
+
+let btnLogout=document.querySelector("#logoutBottone");
+
+
+
+let logoutModale = document.querySelector("#logoutModal");
+
+var closeButtons = document.querySelectorAll('.btn-close');
+
+    for (let i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].addEventListener('click', function() {
+            logoutModale.style.display = "none";
+        });
+    };
+
+
+    btnLogout.addEventListener("click",logOut)
+
 
 }
