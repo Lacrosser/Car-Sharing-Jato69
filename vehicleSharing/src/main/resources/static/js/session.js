2414ee02f95d566a362a4d9f23fa2e38c8e3e130
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2023-01-09 11:29:16 +0100

    Update session.js

--- vehicleSharing/src/main/resources/static/js/session.js
+++ vehicleSharing/src/main/resources/static/js/session.js
@@ -128,7 +128,7 @@ function loginBuilder() {
 function modalBuilder(){
   let htmlCode = '';
 
-  htmlCode=`<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  htmlCode+=`<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
           <div class="modal-header bg-danger">
